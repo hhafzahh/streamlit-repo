ec2430e7828c971123f8b112cd6db76716a1d691
hhafzahh <2001231F@student.tp.edu.sg>
2025-02-12 08:59:41 +0800

    Updated app.py

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ import os
 import streamlit as st
 from google import genai
 
+st.set_page_config("SUTD AI WORKSHOP")
 st.title("Hello world")
 
+client = genai.Client(api_key="YOUR-API-KEY") 
+ 
+response = client.models.generate_content( 
+    model='gemini-2.0-flash',  
+    contents='Tell me what is AI in one paragraph' 
+) 
+ 
+st.write(response.text)
 
 
+qns = st.text_input(label = "Ask Gemini Question")
 
+if st.button("Submit"):
+    response2 = client.models.generate_content(
+        model = "gemini-2.0-flash",
+        contents = qns
+    )
+    
+    st.write(response2.text)
